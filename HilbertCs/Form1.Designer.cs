c43f128d1001bffffccd9a5dd8a6657cae4a783a
junkissa <mosbilous@gmail.com>
2015-06-26 19:33:44 +0300

    Hilbert curve from 0 to 10 order

---
+++ HilbertCs/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Hilbert_XYF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(2, 1);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(510, 510);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(531, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(559, 207);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 244);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(568, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Порядок кривої";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 513);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HilbertCs/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace Hilbert_XYF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+        }
+
+        private static string axiom = "X";
+        private static string newX = "-YF+XFX+FY-";
+        private static string newY = "+XF-YFY-FX+";
+        private static int angle = 90;
+        private static int d_angle = 90;
+        private static int level = 0;
+        private static double step  = 0;
+        private static double X0 = 0, Y0 = 0;
+        void Draw_Fractal(ref string  xx, int l )
+        {
+          //  Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+           // Gl.glClearColor(1, 1, 1, 1);
+           // Gl.glColor3b(0, 0, 0);
+
+            if (l > 0)
+            {               
+                foreach (char c in xx)
+
+                    switch (c)
+                    {
+                        case '+':
+                            {
+                                angle -= d_angle;
+                                break;
+                            }
+                        case '-':
+                            {
+                                angle += d_angle;
+                                break;
+                            }
+                        case 'F':
+                            {
+                                Gl.glBegin(Gl.GL_LINES);
+                                Gl.glColor3d(0.5, 0, 0.5);
+                                Gl.glVertex2d(X0, Y0);
+                                Gl.glVertex2d(X0 + step * Math.Cos(angle * Math.PI / 180), Y0 + step * Math.Sin(angle * Math.PI / 180));
+                                Gl.glEnd();
+                                X0 = X0 + step * Math.Cos(angle * Math.PI / 180);
+                                Y0 = Y0 + step * Math.Sin(angle * Math.PI / 180);
+
+                                break;
+                            }
+                        case 'X':
+                            {
+                                if (l == 1) break;
+                                
+                                Draw_Fractal(ref newX, l-1);
+                                break;
+                            }
+                        case 'Y':
+                            {
+                                if (l == 1) break;
+                               // --level;
+                                Draw_Fractal(ref newY, l - 1);
+                                break;
+                            }
+                        default:
+                                break;
+                    }
+
+
+            }
+            else return;
+
+            //Gl.glFlush();
+            //AnT.Invalidate();
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // инициализация библиотеки GLUT 
+            Glut.glutInit();
+            // инициализация режима окна 
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+
+            // устанавливаем цвет очистки окна 
+            Gl.glClearColor(255, 255, 255, 1);
+            Glu.gluOrtho2D(0.0,  AnT.Width, 0.0, AnT.Height);
+
+            // устанавливаем порт вывода, основываясь на размерах элемента управления AnT 
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            // устанавливаем проекционную матрицу 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очищаем ее 
+            Gl.glLoadIdentity();
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+              Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+             Gl.glClearColor(1, 1, 1, 1);
+            X0 = AnT.Width - 5; Y0 = 3;
+            step = (AnT.Width - 10) / (Math.Pow(2, level) - 1);
+            Draw_Fractal(ref newX, level);
+
+            Gl.glFlush();
+            AnT.Invalidate();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // генерация коэфицента 
+            level = trackBar1.Value;
+            // вывод значения коэфицента, управляемого данным ползунком. 
+            // (под TrackBa'ом) 
+            label1.Text = level.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
